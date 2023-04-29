da1cb4ddc0afefb2a4ad5b741ec14e443b15836c
shashankmadaan379 <codebreaker379@gmail.com>
2023-04-30 01:21:02 +0530

    created form

--- formvalidation/src/Form.js
+++ formvalidation/src/Form.js
@@ -1,5 +1,242 @@
+import React from "react";
+import { GlobalStyle } from "./globalStyle";
+import styled from "styled-components";
 function Form() {
-  return <div className="Form">Form</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <Container>
+        <div className="container">
+          <div className="modal">
+            <div className="modal-container">
+              <div className="modal-left">
+                <h1 className="modal-title">Welcome!</h1>
+                <form>
+                  <div className="input-block">
+                    <label htmlFor="name" className="input-label">
+                      Name
+                    </label>
+                    <input
+                      type="name"
+                      autoComplete="off"
+                      name="name"
+                      placeholder="Name"
+                      id="name"
+                    />
+                  </div>
+                  <div className="input-block">
+                    <label htmlFor="email" className="input-label">
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      autoComplete="off"
+                      name="email"
+                      placeholder="Email"
+                      id="email"
+                    />
+                  </div>
+                  <div className="input-block">
+                    <label htmlFor="password" className="input-label">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      autoComplete="off"
+                      name="password"
+                      placeholder="Password"
+                      id="password"
+                    />
+                  </div>
+                  <div className="modal-buttons">
+                    <a href="#404" className="">
+                      Want to register using gmail
+                    </a>
+                    <button type="submit" className="input-button">
+                      Register
+                    </button>
+                  </div>
+                </form>
+                <p className="sign-up">
+                  Already have an account? <a href="#404">Sign In now</a>
+                </p>
+              </div>
+              <div className="modal-right">
+                <img
+                  src="https://images.unsplash.com/photo-1512486130939-2c4f79935e4f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=dfd2ec5a01006fd8c4d7592a381d3776&auto=format&fit=crop&w=1000&q=80"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
 }
+
+const Container = styled.section`
+  .container {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #efedee;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .modal {
+    width: 100%;
+    /* height: 60px; */
+    background: rgba(51, 51, 51, 0.5);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    transition: 0.4s;
+  }
+  .modal-container {
+    display: flex;
+    max-width: 60vw;
+    width: 100%;
+    border-radius: 10px;
+    overflow: hidden;
+    position: absolute;
+
+    transition-duration: 0.3s;
+    background: #fff;
+  }
+  .modal-title {
+    margin: 0;
+    font-weight: 400;
+    color: #55311c;
+  }
+  .form-error {
+    font-size: 1.4rem;
+    color: #b22b27;
+  }
+  .modal-desc {
+    margin: 6px 0 30px 0;
+  }
+  .modal-left {
+    padding: 60px 30px 20px;
+    background: #fff;
+    flex: 1.5;
+    transition-duration: 0.5s;
+    opacity: 1;
+  }
+
+  .modal-right {
+    flex: 2;
+    font-size: 0;
+    transition: 0.3s;
+    overflow: hidden;
+  }
+  .modal-right img {
+    width: 100%;
+    height: 100%;
+    transform: scale(1);
+    -o-object-fit: cover;
+    object-fit: cover;
+    transition-duration: 1.2s;
+  }
+
+  .modal.is-open .modal-left {
+    transform: translateY(0);
+    opacity: 1;
+    transition-delay: 0.1s;
+  }
+  .modal-buttons {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .modal-buttons a {
+    color: rgba(51, 51, 51, 0.6);
+    font-size: 14px;
+  }
+
+  .sign-up {
+    margin: 60px 0 0;
+    font-size: 14px;
+    text-align: center;
+  }
+  .sign-up a {
+    color: #8c7569;
+  }
+
+  .input-button {
+    padding: 1.2rem 3.2rem;
+    outline: none;
+    text-transform: uppercase;
+    border: 0;
+    color: #fff;
+    border-radius: 4px;
+    background: #8c7569;
+    transition: 0.3s;
+    cursor: pointer;
+    font-family: "Nunito", sans-serif;
+  }
+  .input-button:hover {
+    background: #55311c;
+  }
+
+  .input-label {
+    font-size: 11px;
+    text-transform: uppercase;
+    font-weight: 600;
+    letter-spacing: 0.7px;
+    color: #8c7569;
+    transition: 0.3s;
+  }
+
+  .input-block {
+    display: flex;
+    flex-direction: column;
+    padding: 10px 10px 8px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    margin-bottom: 20px;
+    transition: 0.3s;
+  }
+  .input-block input {
+    outline: 0;
+    border: 0;
+    padding: 4px 0 0;
+    font-size: 14px;
+  }
+
+  .input-block input::-moz-placeholder {
+    color: #ccc;
+    opacity: 1;
+  }
+  .input-block input:-ms-input-placeholder {
+    color: #ccc;
+    opacity: 1;
+  }
+  .input-block input::placeholder {
+    color: #ccc;
+    opacity: 1;
+  }
+  .input-block:focus-within {
+    border-color: #8c7569;
+  }
+  .input-block:focus-within .input-label {
+    color: rgba(140, 117, 105, 0.8);
+  }
+
+  @media (max-width: 750px) {
+    .modal-container {
+      max-width: 90vw;
+    }
+
+    .modal-right {
+      display: none;
+    }
+  }
+`;
 
 export default Form;
