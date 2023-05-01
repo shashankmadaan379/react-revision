c62589c134f48073daa3c90a379b2e4de7d9c179
shashankmadaan379 <codebreaker379@gmail.com>
2023-05-01 15:43:10 +0530

    installed material ui package

--- imagegalleryapp/src/components/imageResult/ImageResult.js
+++ imagegalleryapp/src/components/imageResult/ImageResult.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { GridList, GridListTile } from "@material-ui/core";
 export default class ImageResult extends Component {
   render() {
     return <div>ok</div>;
